ae6803bef6ace7312fc1060a06403a7eb749f6a0
tomashapl <tomas.hapl@gmail.com>
2025-02-03 11:35:42 +0100

    feat: init

---
+++ designer.js
@@ -0,0 +1,29 @@
+function updatePositions() {
+  $('[data-element]').each(function () {
+    const $element = $(this)
+    const width = $element.width()
+    const height = $element.height()
+
+    const originalWidth = parseInt($element.attr("data-original-width"))
+    const originalHeight = parseInt($element.attr("data-original-height"))
+
+    const scaleX = width / originalWidth;
+    const scaleY = height / originalHeight;
+
+    $element.find("[data-marker]").each(function () {
+      const $marker = $(this)
+      const x = parseFloat($marker.attr("data-x"))
+      const y = parseFloat($marker.attr("data-y"))
+      $marker.css("top", y * scaleY - 10)
+      $marker.css("left", x * scaleX - 12)
+    })
+  })
+}
+
+jQuery(document).ready(function () {
+  updatePositions();
+});
+
+window.addEventListener('resize', () => {
+  updatePositions();
+});
---
+++ init.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "https://tomashapl.github.io/axello-designer-export/designer.js";
+  $("body").append(script)
+})
